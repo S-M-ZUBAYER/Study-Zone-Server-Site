6d56f62684330edf13c2e31db638e1f103e0396a
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-10-25 12:40:00 +0600

    Process complete to show the data in clint site

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const app = express()
+const app = express();
+const courses = require('./data/courses.json');
 const port = process.env.port || 5000;
 const cors = require('cors')
 
 app.use(cors());
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('API data processing!')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Showing the data on ${port}`)
 })
