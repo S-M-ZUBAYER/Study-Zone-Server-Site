6b77017a3f9d4844a84da246cbef1f8c117b6043
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-10-27 20:41:51 +0600

    Create another dynamic route

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.get('/courses/:id', (req, res) => {
     const course = courses.find(item => item.id == courseId);
     res.send(course)
 })
+app.get('/items/:course_id', (req, res) => {
+    const courseId = req.params.course_id;
+    const course = courses.find(item => item.course_id == courseId);
+    res.send(course)
+})
 
 app.listen(port, () => {
     console.log(`Showing the data on ${port}`)
